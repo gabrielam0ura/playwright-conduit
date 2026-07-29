7e3afc644b5c39622d91584f572b55c18fbcc6ee
gabrielam0ura <gabrielamoura@alu.ufc.br>
2026-07-28 22:52:50 -0300

    ci: configura ambiente para execução dos testes e2e

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,9 +3,9 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
 
-  fullyParallel: true,
+  fullyParallel: false,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   workers: 1,
 
   reporter: 'html',
@@ -13,6 +13,7 @@ export default defineConfig({
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
+    headless: true,
   },
 
   webServer: {
@@ -27,12 +28,6 @@ export default defineConfig({
       name: 'chromium',
       use: {
         ...devices['Desktop Chrome'],
-      },
-    },
-    {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
       },
     },
   ],
